f9e48545888dc772dde1973ec8fdc512214995ed
Eric Bayasgalan <ewiz1020@gmail.com>
2020-01-13 16:45:50 -0800

    updated the description and minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,5 @@ module.exports = {
     //     display: `minimal-ui`
     //   }
     // }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ]
 };
