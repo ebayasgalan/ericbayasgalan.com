f285966fd6dfdc95d51ed1591c6c14558f03bb2b
Eric Bayasgalan <ewiz1020@gmail.com>
2020-01-18 22:35:19 -0800

    module update

--- src/components/About.js
+++ src/components/About.js
@@ -92,8 +92,8 @@ const About = () => {
           and hardwork. I'm always up for learning new things.
         </p>
         <p>
-          Shortly after graduating college, I got into web development and I
-          love it. I have 3+ years of experience in building exceptional
+          Shortly after graduating college, I started learning web development
+          and I love it. I have 3+ years of experience in building exceptional
           websites and applications. Here are a few technologies I've been
           working with:
         </p>
