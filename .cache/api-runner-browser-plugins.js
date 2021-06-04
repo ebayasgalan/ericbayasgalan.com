a20be114160faf18939795022dc084fbd29b1c5d
Eric Bayasgalan <ewiz1020@gmail.com>
2021-06-04 18:43:09 -0500

    Changed gatsby-plugin-manifest version

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,7 +1,4 @@
 module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"public/icons/icon-144x144.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"6aa69fa27c3ba0f4546169dc6e244df4"},
-    },{
       plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- gatsby-config.js
+++ gatsby-config.js
@@ -10,18 +10,18 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`, 
-        icon: 'public/icons/icon-144x144.png'
-      }
-    }
+    `gatsby-plugin-sharp`
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`, 
+    //     icon: 'public/icons/icon-144x144.png'
+    //   }
+    // }
   ]
 };
